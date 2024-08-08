4b8f1de621b68c88d542fea61b038d9d79e31dab
Randhir Kumar Yadav <yrandhir@outlook.com>
2024-08-07 20:07:48 -0500

    Added ContactsAPI project and removed default code.

---
+++ ContactsAPI/ContactsAPI/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.Run();
