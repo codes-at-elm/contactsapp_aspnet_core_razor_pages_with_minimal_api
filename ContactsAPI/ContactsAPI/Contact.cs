bb488acbeee8f53a590272f1a65d8daf65988e3f
Randhir Kumar Yadav <yrandhir@outlook.com>
2024-08-07 20:35:08 -0500

    Added Contact model.

---
+++ ContactsAPI/ContactsAPI/Contact.cs
@@ -0,0 +1,14 @@
+﻿public class Contact
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string? LastName { get; set; }
+    public string? Company { get; set; }
+    public string? Website { get; set; }
+    public string? Title { get; set; }
+    public int Phone { get; set; }
+    public string? Email { get; set; }
+    public string? AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+}
