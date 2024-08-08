ce09699385b9985f503e68645824e0e03a4f63a9
Randhir Kumar Yadav <yrandhir@outlook.com>
2024-08-07 21:03:08 -0500

    1. Modified Contact model to change datatype of Phone property from int to string. 2. Added ContactDb database context class and added OnModelCreating method to ContactDb to initialize in-memory database with some default contacts to start with for the purpose of demonstration.

--- ContactsAPI/ContactsAPI/Contact.cs
+++ ContactsAPI/ContactsAPI/Contact.cs
@@ -7,7 +7,7 @@
     public string? Company { get; set; }
     public string? Website { get; set; }
     public string? Title { get; set; }
-    public int Phone { get; set; }
+    public string? Phone { get; set; }
     public string? Email { get; set; }
     public string? AddressLine1 { get; set; }
     public string? AddressLine2 { get; set; }
---
+++ ContactsAPI/ContactsAPI/ContactDb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ContactDb : DbContext
+{
+    public ContactDb(DbContextOptions<ContactDb> options) : base(options) { }
+
+    public DbSet<Contact> Contacts => Set<Contact>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Contact>().HasData(
+            new Contact { 
+                FirstName = "Ankita", 
+                LastName="Lokhande", 
+                Company = "AECC Food Mart",
+                Website = "www.aeccfoodmart.com",
+                Title = "Floor Supervisor",
+                Phone = "6039060126",
+                Email = "floormanager@aeccfoodmart.com",
+                AddressLine1 = "5123 Elm Street",
+                AddressLine2 = "Vermillion, South Dakota 57069, USA"
+            },
+            new Contact
+            {
+                FirstName = "Rod",
+                LastName = "Anthony",
+                Company = "Family Snacks LLC",
+                Website = "www.familysnacks.com",
+                Title = "Sr. Application Officer",
+                Phone = "2563905673",
+                Email = "supportadmissions@familysnacks.com",
+                AddressLine1 = "645 Winchester Road",
+                AddressLine2 = "New Market 4567, Alabama"
+            }
+        );
+    }
+}
