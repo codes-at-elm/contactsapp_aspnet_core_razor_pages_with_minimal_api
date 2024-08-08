de2dfa392c42d58542ce9d5ad311f1938ba270a2
Randhir Kumar Yadav <yrandhir@outlook.com>
2024-08-07 21:19:31 -0500

    Modified Program.cs file to add support for EntityFrameworkCore InMemory Database. Added options to SwaggerGen to display Title, Version and Description of the Swagger description. Added code to make sure database is created during start of the application.

--- ContactsAPI/ContactsAPI/Program.cs
+++ ContactsAPI/ContactsAPI/Program.cs
@@ -1,14 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ContactDb>(opt => opt.UseInMemoryDatabase("ContactList"));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+
+    options.SwaggerDoc("", new OpenApiInfo
+    {
+        Title = "Contact API",
+        Version = "v1",
+        Description = "API for managing a list of contacts."
+    });
+});
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ContactDb>();
+    dbContext.Database.EnsureCreated();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
