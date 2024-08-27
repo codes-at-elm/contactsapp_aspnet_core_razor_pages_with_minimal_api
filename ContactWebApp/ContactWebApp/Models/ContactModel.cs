89d77906e0c9b937c064ff52b17ae88f21bceb2e
Randhir Kumar Yadav <yrandhir@outlook.com>
2024-08-26 23:06:30 -0500

    Added model validation attributes to properties of the model.

--- ContactWebApp/ContactWebApp/Models/ContactModel.cs
+++ ContactWebApp/ContactWebApp/Models/ContactModel.cs
@@ -9,27 +9,44 @@ public class ContactModel
     public int Id { get; set; }
 
     [Required]
+    [StringLength(50)]
     [Display(Name = "First Name")]
     public string? FirstName { get; set; }
 
+    [StringLength(50)]
     [Display(Name = "Middle Name")]
     public string? MiddleName { get; set; }
 
     [Required]
+    [StringLength(50)]
     [Display(Name = "Last Name")]
     public string? LastName { get; set; }
-    public string FullName { get => FirstName + (string.IsNullOrEmpty(MiddleName) ? "" : " " + MiddleName) + " " + LastName; }  
-    public string? Company { get; set; }
-    public string? Website { get; set; }
-    public string? Title { get; set; }
+    public string FullName { get => FirstName + (string.IsNullOrEmpty(MiddleName) ? "" : " " + MiddleName) + " " + LastName; }
     
+    [StringLength(100)]
+    public string? Company { get; set; }
+
+    [StringLength(50)]
+    [Url]
+    public string? Website { get; set; }
+
+    [StringLength(50)]
+    public string? Title { get; set; }
+
     [Required]
+    [StringLength(14)]
+    [Phone]
     public string? Phone { get; set; }
+
+    [StringLength(50)]
+    [EmailAddress]
     public string? Email { get; set; }
 
     [Required]
+    [StringLength(50)]
     public string? AddressLine1 { get; set; }
 
     [Required]
+    [StringLength(50)]
     public string? AddressLine2 { get; set; }
 }
